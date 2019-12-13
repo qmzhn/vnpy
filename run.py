f03327869dec055b6fc80d07b15090d70d9edc4d
qmzhn <39551799+qmzhn@users.noreply.github.com>
2019-12-13 13:51:08 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ from vnpy.trader.ui import MainWindow, create_qapp
 from vnpy.gateway.ctp import CtpGateway
 from vnpy.app.cta_strategy import CtaStrategyApp
 from vnpy.app.cta_backtester import CtaBacktesterApp
+import matplotlib
+
+matplotlib.use("Agg")
 
 def main():
     """Start VN Trader"""
